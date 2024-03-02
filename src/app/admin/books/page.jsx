ef982af713bff6e622c877d235a772622c0d92b2
Leonardo <116222412+leopsdev@users.noreply.github.com>
2024-03-02 17:23:02 -0300

    css de admin bar

--- src/app/admin/books/page.jsx
+++ src/app/admin/books/page.jsx
@@ -1,10 +1,12 @@
 import AdminHeaderBar from "@/components/admin-header-bar";
+import { Button } from "@/components/ui/button";
 
 export default function BooksAdminPage() {
   return (
     <>
       <AdminHeaderBar />
       <main>
+        <Button>Adcionar Livro</Button>
         <h1>Livros Admin</h1>
       </main>
     </>
--- src/components/admin-header-bar.jsx
+++ src/components/admin-header-bar.jsx
@@ -13,18 +13,20 @@ import { Button } from "@/components/ui/button";
 
 export default function AdminHeaderBar() {
   return (
-    <header className="flex h-16 items-center justify-between border-b-[1px] px-4">
-      <Link href="/home">Logo</Link>
+    <header className="flex h-16 items-center justify-between border-b-[1px] px-4 bg-[#A0DFC7]">
+      <Link href="/home">
+      <img src="/logoNormal.svg" alt="" />
+      </Link>
       <div className="flex flex-row gap-4">
         <Sheet>
           <SheetTrigger>
-            <HamburgerMenuIcon className="h-6 w-6"></HamburgerMenuIcon>
+            <HamburgerMenuIcon className="h-6 w-6 text-[#113535]"></HamburgerMenuIcon>
           </SheetTrigger>
-          <SheetContent className="flex flex-col justify-between">
+          <SheetContent className="flex flex-col justify-between bg-[#113535] text-white border-0">
             <div className="flex flex-col gap-4">
               <SheetHeader>
-                <SheetTitle>Admin</SheetTitle>
-                <SheetDescription>
+                <SheetTitle className=" text-white">Admin</SheetTitle>
+                <SheetDescription className=" text-white">
                   Gerencie os dados da biblioteca
                 </SheetDescription>
               </SheetHeader>
@@ -34,7 +36,7 @@ export default function AdminHeaderBar() {
               <Link href="/admin/users">Usuários</Link>
             </div>
             <SheetFooter>
-              <Button>Sair</Button>
+              <Button className="bg-[#FF2E2E] hover:bg-red-800 ">Sair</Button>
             </SheetFooter>
           </SheetContent>
         </Sheet>
